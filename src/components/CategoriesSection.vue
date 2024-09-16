13bdeae45888bedce7042f8864485b5bff6ee6de
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-16 18:55:27 +0300

    Make CardItem reusable component

--- src/components/CategoriesSection.vue
+++ src/components/CategoriesSection.vue
@@ -1,53 +1,17 @@
 <script setup>
-import Card from 'primevue/card';
-import Button from 'primevue/button'
+import CardItem from './UI/CardItem.vue';
 import phoneHolder from '../assets/accessories/phone-holder.webp'
 import steeringWheel from '../assets/accessories/steering-wheel.webp'
-// import duck from '../assets/accessories/duck.webp'
-import CardItem from './UI/CardItem.vue';
+import duck from '../assets/accessories/duck.webp'
 </script>
 
 <template>
   <div id="categories">
   <h2>Categories</h2>
   <div id="card-collection">
-    <Card class="card" style="width: 25rem; overflow: hidden">
-    <template #header>
-        <img alt="user header" :src="steeringWheel" />
-    </template>
-    <template #title>Interior</template>
-    <template #subtitle>Car Accessories</template>
-    <template #footer>
-      <div class="flex gap-4 mt-1">
-        <Button label="Review" class="w-full" />
-      </div>
-    </template>
-  </Card>
-  <Card class="card" style="width: 25rem; overflow: hidden">
-    <template #header>
-        <img alt="user header" :src="phoneHolder" />
-    </template>
-    <template #title>Exterior</template>
-    <template #subtitle>Car Accessories</template>
-    <template #footer>
-      <div class="flex gap-4 mt-1">
-        <Button label="Review" class="w-full" />
-      </div>
-    </template>
-  </Card>
-  <!-- <Card class="card" style="width: 25rem; overflow: hidden">
-    <template #header>
-        <img alt="user header" :src="duck" />
-    </template>
-    <template #title>Additional Stuff</template>
-    <template #subtitle>Car Accessories</template>
-    <template #footer>
-      <div class="flex gap-4 mt-1">
-        <Button label="Review" class="w-full" />
-      </div>
-    </template>
-  </Card> -->
-  <CardItem title="test" subtitle="text"/>
+    <CardItem :img="steeringWheel" title="Interior" subtitle="Car Accessories"/>
+    <CardItem :img="phoneHolder" title="Exterior" subtitle="Car Accessories"/>
+    <CardItem :img="duck" title="Additional Stuff" subtitle="Car Accessories"/>
   </div>
 </div>
 </template>
--- src/components/UI/CardItem.vue
+++ src/components/UI/CardItem.vue
@@ -3,13 +3,13 @@ import Card from 'primevue/card';
 import Button from 'primevue/button'
 import { defineProps } from 'vue';
 
-const props = defineProps(['src', 'title', 'subtitle'])
+const props = defineProps(['img', 'title', 'subtitle'])
 </script>
 
 <template>
  <Card class="card" style="width: 25rem; overflow: hidden">
     <template #header>
-        <img alt="user header" :src="src" />
+        <img alt="user header" :src="img" />
     </template>
     <template #title>{{props.title}}</template>
     <template #subtitle>{{props.subtitle}}</template>
@@ -22,5 +22,48 @@ const props = defineProps(['src', 'title', 'subtitle'])
 </template>
 
 <style scoped>
+  #categories {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
+  #card-collection {
+    margin-top: 15px;
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+  }
+
+  @media only screen and (max-width: 1270px) and (min-width: 881px) {
+    .card {
+      max-width: 280px;
+    }
+  }
+
+  @media only screen and (max-width: 500px) {
+    .card {
+      max-width: 250px;
+    }
+  }
+
+  @media only screen and (max-width: 880px) {
+    #card-collection {
+      flex-direction: column;
+    }
+  }
+
+  .card {
+    margin: 5px;
+  }
+  
+  img {
+    width: 100%;
+  }
+
+  h2 {
+    margin-top: 30px;
+    color: black;
+  }
 </style>
