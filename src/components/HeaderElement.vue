bbc26571b15ee4b363707a4ad0863d96842b7302
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-16 17:08:40 +0300

    add responsivness to the design of the header

--- src/components/HeaderElement.vue
+++ src/components/HeaderElement.vue
@@ -1,30 +1,83 @@
 <script setup>
-
 </script>
 
 <template>
   <header>
     <h1>
-      Car Accessories
+      <router-link id="title" :to="{ name: 'Home'}">Car Accessories</router-link>
+     
     </h1>
-    <ul>
-      <li>
-        <router-link class="link" :to="{ name: 'Home'}">Home</router-link>
-      </li>
-      <li>
-        <router-link class="link"  :to="{ name: 'Interior'}">Interior</router-link>
-      </li>
-      <li>
-        <router-link class="link"  :to="{ name: 'Exterior'}">Exterior</router-link> 
-      </li>
-      <li>
-        <router-link class="link"  :to="{ name: 'Contacts'}">Contacts</router-link>
-      </li>
-    </ul>
+      <div class="dropdown">
+        <button>Pages</button>
+        <div class="content">
+          <ul>
+            <li>
+              <router-link class="link" :to="{ name: 'Home'}">Home</router-link>
+            </li>
+            <li>
+              <router-link class="link"  :to="{ name: 'Interior'}">Interior</router-link>
+            </li>
+            <li>
+              <router-link class="link"  :to="{ name: 'Exterior'}">Exterior</router-link> 
+            </li>
+            <li>
+              <router-link class="link"  :to="{ name: 'Contacts'}">Contacts</router-link>
+            </li>
+        </ul>    
+        </div>
+      </div>
   </header>
 </template>
 
 <style scoped>
+  .dropdown button {
+    display: none;
+  }
+  @media only screen and (max-width: 350px) {
+    .dropdown {
+    z-index: 1;
+    margin-top: 15px;
+    width: 100px;
+    }
+
+    .dropdown button {
+      display: flex;
+      justify-content: center;
+      border-radius: 5px;
+      width: 80px;
+      background-color: hsl(0, 0%, 80%);
+      padding: 10px 15px;
+      border: none;
+      cursor: pointer;;
+    }
+    .dropdown ul {
+      display: block;
+      color: black;
+      width: 80px;
+      border-radius: 10px;
+      padding: 10px 15px;
+    }
+    .dropdown .content {
+      display: none;
+      position: absolute;
+      background-color: hsl(0, 0%, 95%);
+      height: 90px;
+      box-shadow: 2px 2px 5px hsla(0, 0%, 0%, 0.808);
+    }
+
+    .dropdown:hover .content {
+      display: block;
+    }
+    .dropdown:hover button {
+      background-color: hsl(0, 0%, 70%);
+    }
+    .dropdown li:hover {
+      background-color: hsl(0,0%, 90%);
+      }
+    }
+  
+
+
   header {
     color: black;
     display: flex;
@@ -44,7 +97,8 @@
     height: 30px;
     align-items: end;
     justify-content: space-around;
-    padding-left: 0px
+    padding-left: 0px;
+    margin: 0px;
   }
   li {
     list-style: none;
@@ -53,10 +107,10 @@
   }
   @media only screen and (max-width: 730px) {
     h1 {
-    font-size: 22px;
+      font-size: 22px;
     }
     ul {
-    width: 16em;
+      width: 16em;
     }
     li {
       font-size: 16px;
@@ -75,7 +129,7 @@
     }
   }
 
-  .link {
+  .link ,#title {
     color: black;
     text-decoration: none;
   }
