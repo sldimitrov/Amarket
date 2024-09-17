ee290e2e4c91b20b2fc3ca099d7552e9f79de393
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-18 00:19:53 +0300

    create the product details component that conditionaly renders the data for every product

--- src/views/ProductDetailsView.vue
+++ src/views/ProductDetailsView.vue
@@ -10,12 +10,76 @@ const currentProduct = products.filter(product => product.service_id === id)
 
 
 <template>
-  <h2>{{ currentProduct[0].title }}</h2>
-  <h3>{{ currentProduct[0].dealer }}</h3>
+  <div id="product-details">
+    <div id="discount">{{currentProduct[0].discount_percentages}}</div>
+    <img :src="currentProduct[0].images[0]" alt="">
+    <div id="product-info">
+      <h2 id="title">{{currentProduct[0].title}}</h2>
+      <h3><span class="label">Dealer:</span> {{currentProduct[0].dealer}}</h3>
+      <h3><span class="label">Service Type:</span> {{currentProduct[0].service_type}}</h3>
+      <h3 id="id-num"><span class="label">Артикулен номер №:</span>&nbsp;{{currentProduct[0].service_id}}</h3>
+      <div id="pricing" v-if="currentProduct[0].discount_percentages != 0">
+        <h2 id="onsale">{{ currentProduct[0].discount_price }}лв.</h2>
+        <h3 id="original-price"><span class="label">&nbsp;{{ currentProduct[0].price }}</span></h3>
+      </div>
+      <h3 v-else id="onsale">
+        {{ currentProduct[0].price }}лв.
+      </h3>
+    </div>
+
+  </div>
 </template>
 
 
 <style scoped>
+  #product-details {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: auto;
+    margin-top: 30px;
+  }
+
+  #product-info {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    margin-left: 10px;
+  }
+
+  #title {
+    border-bottom: 1px solid #e8e8e8;
+    padding-bottom: 15px;
+  }
+
+  #pricing {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100px;
+  }
+
+  #onsale {
+    font-size: 35px;
+  }
+
+  #id-num {
+    color: #337ab7;
+  }
+
+  #original-price {
+    font-size: 23px;
+    text-decoration: line-through;
+  }
+
+  .label {
+    color: #999;
+  }
+
+  img {
+    max-width: 550px;
+  }
+
   h2, h3 {
     color: black;
     text-align: center;
