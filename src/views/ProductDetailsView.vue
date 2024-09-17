e282842b21f6e2a192f1d65b139eefc9b20b2ab9
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-09-17 23:30:31 +0300

    fetch the data and show the details of the current product

--- src/views/ProductDetailsView.vue
+++ src/views/ProductDetailsView.vue
@@ -1,15 +1,22 @@
 <script setup>
+import { useRoute } from 'vue-router';
+import productsData from '@/data/products.json'
+const products = productsData;
 
+const route = useRoute()
+const id = route.params.id
+const currentProduct = products.filter(product => product.service_id === id)
 </script>
 
 
 <template>
-  <h2>Product {{ $route.params.id }}</h2>
+  <h2>{{ currentProduct[0].title }}</h2>
+  <h3>{{ currentProduct[0].dealer }}</h3>
 </template>
 
 
 <style scoped>
-  h2 {
+  h2, h3 {
     color: black;
     text-align: center;
   }
